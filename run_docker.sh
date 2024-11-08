e904f8ed76f55f0be490ba2ab16163edcdc894ad
otischung <p76124605@gs.ncku.edu.tw>
2024-11-08 15:00:53 +0800

    [FEAT] Add device options in run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+device_options=""
+
+if [ -e /dev/ttyUSB0 ]; then
+    device_options+=" --device=/dev/ttyUSB0"
+fi
+
+if [ -e /dev/ttyACM0 ]; then
+    device_options+=" --device=/dev/ttyACM0"
+fi
+
 docker run -it --rm \
-    -v ./src:/workspaces/src \
+    -v ./src/arm_control:/workspaces/src/arm_control \
+    --network host \
+    $device_options \
+    --env-file ./.env \
     registry.screamtrumpet.csie.ncku.edu.tw/pros_images/pros_base_image:latest \
     /bin/bash
