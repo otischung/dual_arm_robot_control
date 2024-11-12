9b1c9a4e6a6081055d4799fdd191ebe4f24d0c37
otischung <p76124605@gs.ncku.edu.tw>
2024-11-12 17:03:17 +0800

    [FEAT] Add change panel select function

--- src/arm_control/arm_control/control_panel.py
+++ src/arm_control/arm_control/control_panel.py
@@ -10,31 +10,44 @@ class ControlPanel():
     def __init__(self, arm_publisher_: ArmPublisher):
         # The transition table of the Finite State Machine
         self._transitions = [
+            # Enter and Esc Keys
             {"src": PanelState.NORMAL, "dst": PanelState.NORMAL,
-                "key": KEY_ENTER, "sel": PanelSelect.RESET},
+                "key": KEY_ENTER, "sel": PanelSelect.RESET, "exec": None},
             {"src": PanelState.NORMAL, "dst": PanelState.SELECT,
-                "key": KEY_ENTER, "sel": PanelSelect.LEFT | PanelSelect.RIGHT},
+                "key": KEY_ENTER, "sel": PanelSelect.LEFT | PanelSelect.RIGHT, "exec": None},
             {"src": PanelState.NORMAL, "dst": PanelState.CONTROL_STEP,
-                "key": KEY_ENTER, "sel": PanelSelect.STEP},
+                "key": KEY_ENTER, "sel": PanelSelect.STEP, "exec": None},
             {"src": PanelState.NORMAL, "dst": PanelState.EXIT,
-                "key": KEY_ESC, "sel": PanelSelect.ALL},
+                "key": KEY_ESC, "sel": PanelSelect.ALL, "exec": None},
             {"src": PanelState.SELECT, "dst": PanelState.NORMAL,
-                "key": KEY_ESC, "sel": PanelSelect.LEFT | PanelSelect.RIGHT},
+                "key": KEY_ESC, "sel": PanelSelect.LEFT | PanelSelect.RIGHT, "exec": None},
             {"src": PanelState.SELECT, "dst": PanelState.CONTROL_JOINT,
-                "key": KEY_ENTER, "sel": PanelSelect.LEFT | PanelSelect.RIGHT},
+                "key": KEY_ENTER, "sel": PanelSelect.LEFT | PanelSelect.RIGHT, "exec": None},
             {"src": PanelState.CONTROL_JOINT, "dst": PanelState.SELECT,
-                "key": KEY_ESC, "sel": PanelSelect.LEFT | PanelSelect.RIGHT},
+                "key": KEY_ESC, "sel": PanelSelect.LEFT | PanelSelect.RIGHT, "exec": None},
             {"src": PanelState.CONTROL_STEP, "dst": PanelState.NORMAL,
-                "key": KEY_ESC, "sel": PanelSelect.STEP},
+                "key": KEY_ESC, "sel": PanelSelect.STEP, "exec": None},
+            # Up and Down Arrow Keys
+            {"src": PanelState.NORMAL, "dst": PanelState.NORMAL,
+                "key": curses.KEY_UP, "sel": PanelSelect.ALL, "exec": self.change_select},
+            {"src": PanelState.NORMAL, "dst": PanelState.NORMAL,
+                "key": curses.KEY_DOWN, "sel": PanelSelect.ALL, "exec": self.change_select},
         ]
         self._cur_state: PanelState = PanelState.NORMAL
         self._cur_sel: PanelSelect = PanelSelect.LEFT
         self._cur_sel_joint: int = 0
 
+    def change_select(self, key: int):
+        if key == curses.KEY_UP:
+            self._cur_sel = prev_bitwise_enum(self._cur_sel, PanelSelect)
+        else:
+            self._cur_sel = next_bitwise_enum(self._cur_sel, PanelSelect)
+
     def control_loop(self, stdscr):
         while True:
             stdscr.clear()
-            stdscr.addstr(1, 0, f"{self._cur_state.name}")
+            stdscr.addstr(1, 0, f"State:\t{self._cur_state.name}")
+            stdscr.addstr(2, 0, f"Select:\t{self._cur_sel.name}")
             key = key_trans(stdscr.getch())
             trans: dict = None
 
@@ -49,6 +62,11 @@ class ControlPanel():
             if trans is None:
                 stdscr.addstr(2, 0, "Error, there is NO matched transition.")
             else:
+                if trans["exec"] is not None:
+                    trans["exec"](key)
+                else:
+                    stdscr.addstr(
+                        3, 0, f"Error, there is no execution function.")
                 self._cur_state = trans["dst"]
             stdscr.refresh()
 
